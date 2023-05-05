9cce61af6180a9d474e29105f2a0cbf641de6b1d
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2023-05-05 11:33:40 +0100

    department findById implementation

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,13 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -28,7 +33,34 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public Department findById(Integer id) {
-		return null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			preparedStatement = connection.prepareStatement(
+					"SELECT Id, Name "
+					+ "FROM department "
+					+ "WHERE Id = ? ");
+			
+			preparedStatement.setInt(1, id);
+			resultSet = preparedStatement.executeQuery();
+			
+			if(resultSet.next()) {
+				Department department = new Department();
+				department.setId(resultSet.getInt("Id"));
+				department.setName(resultSet.getString("Name"));
+				
+				return department;
+			}
+			
+			return null;
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+		}
+		
 	}
 
 	@Override
