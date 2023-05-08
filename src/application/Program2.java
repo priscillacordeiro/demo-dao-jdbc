ded221ea2717c401db0d23c871490e298688bd98
Priscilla Cordeiro <priscillacordeiros@gmail.com>
2023-05-08 17:21:06 +0100

    department update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -16,18 +16,24 @@ public class Program2 {
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
 		
-		System.out.println("===== TEST 2: findAll =====");
+		System.out.println("\n===== TEST 2: findAll =====");
 		List<Department> list = departmentDao.findAll();
 
 		for (Department element : list) {
 			System.out.println(element);
 		}
 		
-		System.out.println("===== TEST 3: department insert =====");
+		System.out.println("\n===== TEST 3: department insert =====");
 		Department newDepartment = new Department(null, "Beauty");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
 
+		System.out.println("\n===== TEST 4: department update =====");
+		Department selectedDepartment = departmentDao.findById(5);
+		selectedDepartment.setName("Animals");
+		departmentDao.update(selectedDepartment);
+		System.out.println("Update completed");
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -58,6 +58,24 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void update(Department department) {
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			preparedStatement = connection.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE Id = ? ");
+			
+			preparedStatement.setString(1, department.getName());
+			preparedStatement.setInt(2, department.getId());
+			
+			preparedStatement.executeUpdate();
+			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(preparedStatement);
+		}
 	}
 
 	@Override
